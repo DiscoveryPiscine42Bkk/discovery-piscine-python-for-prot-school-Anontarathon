9f1db3940630a7754f411702e8a9a9e39ea6ef84
Anontarathon <40319@prot.ac.th>
2025-03-10 15:54:17 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,9 @@
+
+password = input("pls enter your password")
+
+inp_password = "phython is a awesome"
+if password == inp_password :
+
+    print("access ganted")
+else : 
+    print("access defind")
