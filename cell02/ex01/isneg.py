a773dc854a9de1d09dd0f16a3c6378293a165203
Anontarathon <40319@prot.ac.th>
2025-03-10 15:39:17 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = int(input("pls input ur num"))
+if number >0 :
+    print("this num is negative")
+elif number <0 : 
+    print("this num is positive")
+elif number ==0 :
+    print("this num is both")
