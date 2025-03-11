6ff68b515539d447691df05468a7314517600ef7
Anontarathon <40319@prot.ac.th>
2025-03-11 11:27:39 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+awnser1 = input("what you gotta say?")
+STOP = True
+while True:
+    STOP = input("i got that! anything else?")
+    if STOP == "STOP":
+        print("อย่าตีกันๆ")
+        break
+    else:
+        print("i ve got it")
