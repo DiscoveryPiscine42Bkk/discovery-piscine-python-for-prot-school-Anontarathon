eb847bae28f597ebc0aeb04766dbb2823ce7a92a
Anontarathon <40319@prot.ac.th>
2025-03-11 15:10:51 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+a = int(input("Give me the frist number"))
+b = int(input("Give me the second number"))
+print("Thank You!")
+print(input(f"{a} + {b} = {a+b}"))
+print(input(f"{a} - {b} = {a-b}"))
+print(input(f"{a} * {b} = {a*b}"))
+print(input(f"{a} / {b} = {a/b}"))
+
+      
