9b1563377a08cce1cc2d067141bdd0490820110f
Anontarathon <40319@prot.ac.th>
2025-03-11 10:10:39 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number less than 25"))
+if number >= 25 :
+    print("error")
+else :
+    while number <= 25 :
+        print(f"inside the loop, mu\y variable is {number}" )
+        number += 1
+
+    
+    
+
