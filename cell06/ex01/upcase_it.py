65ae023aaf74c91a03bd4c7c8edd6bfb6ec6bec7
Anontarathon <40319@prot.ac.th>
2025-03-13 22:21:52 +0700

    Add files via upload

---
+++ cell06/ex01/upcase_it.py
@@ -0,0 +1,8 @@
+
+def upccase_it(a): #กำหนดค่อ a (1)
+    return a.lower()
+print(upccase_it("HELLO")) #สั่งให้ปริ้น แต่ต้องกลับไปพิจารณาต้อง return แล้วจะปริ้นตัว ค่าที่อยู่ใน print ตามฟังชันที่กำหนดใน return
+
+    
+
+       
