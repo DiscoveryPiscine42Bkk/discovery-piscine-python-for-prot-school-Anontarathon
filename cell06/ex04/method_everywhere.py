24293197f6863a01c6f10f23d87294d1f3163ced
Anontarathon <40319@prot.ac.th>
2025-03-14 13:39:35 +0700

    Add files via upload

---
+++ cell06/ex04/method_everywhere.py
@@ -0,0 +1,9 @@
+import sys 
+if len(sys.argv) != 4:
+    print("none")
+else:
+    print(sys.argv[1]+"ZZZZ"+"$")
+    print(sys.argv[2][:8]+"$")
+    print(sys.argv[3]+"$")
+def method():
+    print('lol' 'physically' 'backpack')
